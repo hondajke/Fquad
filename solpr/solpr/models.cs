93f825af5ae97c15cdbf1fff8830d561c361cb93
NEFUstudent <study.svfu@gmail.com>
2020-02-21 12:36:18 +0900

    Added classes Manufacturer, Component

--- solpr/solpr/models.cs
+++ solpr/solpr/models.cs
@@ -3,10 +3,28 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace solpr
 {
-    class models
+    enum ComponentType { video, processor, rom, disk }
+    class Manufacturer
     {
+        [Key]
+        public string Name { get; set; }
+        public ICollection<Component> Components { get; set; }
     }
+
+    class Component
+    {
+        [Key]
+        public int Id { get; set; }
+        public ComponentType Type { get; set; }
+        public int ManufacturerId { get; set; }
+        [ForeignKey("ManufacturerId")]
+        public Manufacturer Manufacturer { get; set; }
+    }
+
+
 }
