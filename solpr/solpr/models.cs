ea1134b9dcdbf349a1afac74dddbd47358399997
NEFUstudent <study.svfu@gmail.com>
2020-02-28 12:00:04 +0900

    Changed models.cs

--- solpr/solpr/models.cs
+++ solpr/solpr/models.cs
@@ -37,6 +37,7 @@ namespace solpr
     class Department
     {
         [Key]
+        public int Id { get; set; }
         public string Name { get; set; }
         public ICollection<Employee> Employees { get; set; }
     }
