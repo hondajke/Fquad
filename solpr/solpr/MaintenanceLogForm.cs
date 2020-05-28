3bccbbc6bb1a42268dfd69813e17d620e7f6b515
plenzke <3njoyth3sil3nc399@gmail.com>
2020-05-28 21:40:45 +0900

    added month filter template

--- solpr/solpr/MaintenanceLogForm.cs
+++ solpr/solpr/MaintenanceLogForm.cs
@@ -8,6 +8,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Data.Entity;
+using System.Linq.Expressions;
 
 namespace solpr
 {
@@ -16,14 +17,29 @@ namespace solpr
         ParkDBEntities db;
         int searchCellNum = 0;
         List<DataGridViewCell> searchCells;
+        string[] months = new string[] { "Январь", "Февраль", "Март", "Апрель", "Май", "Июнь", "Июль", "Август", "Сентябрь", "Октябрь", "Ноябрь", "Декабрь" };
 
         public MaintenanceLogForm()
         {
             InitializeComponent();
             db = new ParkDBEntities();
+            comboBox1.Items.AddRange(months);
 
             RefreshMainLogGrid();
 
+        }
+
+        public Expression<Func<TElement, bool>> IsSameDate<TElement>(Expression<Func<TElement, DateTime>> valueSelector, DateTime value)
+        {
+            ParameterExpression p = valueSelector.Parameters.Single();
+
+            var antes = Expression.GreaterThanOrEqual(valueSelector.Body, Expression.Constant(value.Date, typeof(DateTime)));
+
+            var despues = Expression.LessThan(valueSelector.Body, Expression.Constant(value.AddDays(1).Date, typeof(DateTime)));
+
+            Expression body = Expression.And(antes, despues);
+
+            return Expression.Lambda<Func<TElement, bool>>(body, p);
         }
 
         public void RefreshMainLogGrid()
@@ -95,6 +111,21 @@ namespace solpr
                 }
                 dataGridView1.CurrentCell = searchCells[searchCellNum];
             }
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            var result = from maint in db.Maintenance
+                         join pc in db.Computers on maint.ComputerId equals pc.Id
+                         select new
+                         {
+                             ID = maint.Id,
+                             PC_ID = pc.Id,
+                             Причина = maint.Description,
+                             Дата_начала_ремонта = maint.RepairStart.Day.ToString() + maint.RepairStart.Month.ToString() + maint.RepairStart.Year.ToString(),
+                             Дата_окончания_ремонта = maint.RepairFinish.Value.Day.ToString() + maint.RepairFinish.Value.Month.ToString() + maint.RepairFinish.Value.Year.ToString()
+                         };
+           
         }
     }
 }
