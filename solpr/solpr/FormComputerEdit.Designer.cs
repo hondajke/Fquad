20279d60198e4de3f30339d5e5f4fb907a4b3ccb
D-iardan <D-iardan@yandex.ru>
2020-06-04 23:20:44 +0900

    computer edit

--- solpr/solpr/FormComputerEdit.Designer.cs
+++ solpr/solpr/FormComputerEdit.Designer.cs
@@ -33,24 +33,17 @@
             this.comboBox8 = new System.Windows.Forms.ComboBox();
             this.label8 = new System.Windows.Forms.Label();
             this.comboBox7 = new System.Windows.Forms.ComboBox();
-            this.comboBox6 = new System.Windows.Forms.ComboBox();
             this.label7 = new System.Windows.Forms.Label();
-            this.label6 = new System.Windows.Forms.Label();
-            this.label5 = new System.Windows.Forms.Label();
-            this.label4 = new System.Windows.Forms.Label();
-            this.label3 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
-            this.comboBox5 = new System.Windows.Forms.ComboBox();
-            this.comboBox4 = new System.Windows.Forms.ComboBox();
-            this.comboBox3 = new System.Windows.Forms.ComboBox();
-            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.dataGridView2 = new System.Windows.Forms.DataGridView();
             this.comboBox1 = new System.Windows.Forms.ComboBox();
-            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(29, 387);
+            this.button1.Location = new System.Drawing.Point(578, 472);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 0;
@@ -60,7 +53,7 @@
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(232, 387);
+            this.button2.Location = new System.Drawing.Point(685, 472);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(75, 23);
             this.button2.TabIndex = 1;
@@ -71,7 +64,7 @@
             // comboBox8
             // 
             this.comboBox8.FormattingEnabled = true;
-            this.comboBox8.Location = new System.Drawing.Point(164, 323);
+            this.comboBox8.Location = new System.Drawing.Point(617, 424);
             this.comboBox8.Name = "comboBox8";
             this.comboBox8.Size = new System.Drawing.Size(143, 21);
             this.comboBox8.TabIndex = 49;
@@ -79,7 +72,7 @@
             // label8
             // 
             this.label8.AutoSize = true;
-            this.label8.Location = new System.Drawing.Point(21, 323);
+            this.label8.Location = new System.Drawing.Point(555, 427);
             this.label8.Name = "label8";
             this.label8.Size = new System.Drawing.Size(41, 13);
             this.label8.TabIndex = 48;
@@ -88,148 +81,65 @@
             // comboBox7
             // 
             this.comboBox7.FormattingEnabled = true;
-            this.comboBox7.Location = new System.Drawing.Point(164, 282);
+            this.comboBox7.Location = new System.Drawing.Point(389, 424);
             this.comboBox7.Name = "comboBox7";
             this.comboBox7.Size = new System.Drawing.Size(143, 21);
             this.comboBox7.TabIndex = 47;
             // 
-            // comboBox6
-            // 
-            this.comboBox6.FormattingEnabled = true;
-            this.comboBox6.Location = new System.Drawing.Point(164, 239);
-            this.comboBox6.Name = "comboBox6";
-            this.comboBox6.Size = new System.Drawing.Size(143, 21);
-            this.comboBox6.TabIndex = 46;
-            // 
             // label7
             // 
             this.label7.AutoSize = true;
-            this.label7.Location = new System.Drawing.Point(21, 282);
+            this.label7.Location = new System.Drawing.Point(307, 427);
             this.label7.Name = "label7";
             this.label7.Size = new System.Drawing.Size(60, 13);
             this.label7.TabIndex = 45;
             this.label7.Text = "Сотрудник";
             // 
-            // label6
+            // dataGridView1
             // 
-            this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(21, 239);
-            this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(102, 13);
-            this.label6.TabIndex = 44;
-            this.label6.Text = "Клавиатура, мышь";
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(28, 22);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(731, 221);
+            this.dataGridView1.TabIndex = 50;
+            this.dataGridView1.SelectionChanged += new System.EventHandler(this.dataGridView1_SelectionChanged);
             // 
-            // label5
+            // dataGridView2
             // 
-            this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(21, 110);
-            this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(67, 13);
-            this.label5.TabIndex = 43;
-            this.label5.Text = "Видеокарта";
-            // 
-            // label4
-            // 
-            this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(21, 196);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(80, 13);
-            this.label4.TabIndex = 42;
-            this.label4.Text = "Жесткий диск";
-            // 
-            // label3
-            // 
-            this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(21, 153);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(114, 13);
-            this.label3.TabIndex = 41;
-            this.label3.Text = "Оперативная память";
-            // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(21, 67);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(59, 13);
-            this.label2.TabIndex = 40;
-            this.label2.Text = "Мат плата";
-            // 
-            // comboBox5
-            // 
-            this.comboBox5.FormattingEnabled = true;
-            this.comboBox5.Location = new System.Drawing.Point(164, 67);
-            this.comboBox5.Name = "comboBox5";
-            this.comboBox5.Size = new System.Drawing.Size(143, 21);
-            this.comboBox5.TabIndex = 39;
-            // 
-            // comboBox4
-            // 
-            this.comboBox4.FormattingEnabled = true;
-            this.comboBox4.Location = new System.Drawing.Point(164, 196);
-            this.comboBox4.Name = "comboBox4";
-            this.comboBox4.Size = new System.Drawing.Size(143, 21);
-            this.comboBox4.TabIndex = 38;
-            // 
-            // comboBox3
-            // 
-            this.comboBox3.FormattingEnabled = true;
-            this.comboBox3.Location = new System.Drawing.Point(164, 110);
-            this.comboBox3.Name = "comboBox3";
-            this.comboBox3.Size = new System.Drawing.Size(143, 21);
-            this.comboBox3.TabIndex = 37;
-            // 
-            // comboBox2
-            // 
-            this.comboBox2.FormattingEnabled = true;
-            this.comboBox2.Location = new System.Drawing.Point(164, 153);
-            this.comboBox2.Name = "comboBox2";
-            this.comboBox2.Size = new System.Drawing.Size(143, 21);
-            this.comboBox2.TabIndex = 36;
+            this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView2.Location = new System.Drawing.Point(28, 273);
+            this.dataGridView2.Name = "dataGridView2";
+            this.dataGridView2.Size = new System.Drawing.Size(731, 124);
+            this.dataGridView2.TabIndex = 51;
             // 
             // comboBox1
             // 
             this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Location = new System.Drawing.Point(164, 24);
+            this.comboBox1.Location = new System.Drawing.Point(138, 424);
             this.comboBox1.Name = "comboBox1";
             this.comboBox1.Size = new System.Drawing.Size(143, 21);
-            this.comboBox1.TabIndex = 35;
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(21, 24);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(63, 13);
-            this.label1.TabIndex = 34;
-            this.label1.Text = "Процессор";
+            this.comboBox1.TabIndex = 52;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
             // 
             // FormComputerEdit
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(334, 432);
+            this.ClientSize = new System.Drawing.Size(788, 507);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.dataGridView2);
+            this.Controls.Add(this.dataGridView1);
             this.Controls.Add(this.comboBox8);
             this.Controls.Add(this.label8);
             this.Controls.Add(this.comboBox7);
-            this.Controls.Add(this.comboBox6);
             this.Controls.Add(this.label7);
-            this.Controls.Add(this.label6);
-            this.Controls.Add(this.label5);
-            this.Controls.Add(this.label4);
-            this.Controls.Add(this.label3);
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.comboBox5);
-            this.Controls.Add(this.comboBox4);
-            this.Controls.Add(this.comboBox3);
-            this.Controls.Add(this.comboBox2);
-            this.Controls.Add(this.comboBox1);
-            this.Controls.Add(this.label1);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Name = "FormComputerEdit";
             this.Text = "FormComputerEdit";
             this.Load += new System.EventHandler(this.FormComputerEdit_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -242,18 +152,9 @@
         private System.Windows.Forms.ComboBox comboBox8;
         private System.Windows.Forms.Label label8;
         private System.Windows.Forms.ComboBox comboBox7;
-        private System.Windows.Forms.ComboBox comboBox6;
         private System.Windows.Forms.Label label7;
-        private System.Windows.Forms.Label label6;
-        private System.Windows.Forms.Label label5;
-        private System.Windows.Forms.Label label4;
-        private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.ComboBox comboBox5;
-        private System.Windows.Forms.ComboBox comboBox4;
-        private System.Windows.Forms.ComboBox comboBox3;
-        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridView dataGridView2;
         private System.Windows.Forms.ComboBox comboBox1;
-        private System.Windows.Forms.Label label1;
     }
 }
--- solpr/solpr/FormComputerEdit.cs
+++ solpr/solpr/FormComputerEdit.cs
@@ -13,7 +13,7 @@ namespace solpr
     public partial class FormComputerEdit : Form
     {
         ParkDBEntities db;
-
+       
         public FormComputerEdit()
         {
             InitializeComponent();
@@ -24,11 +24,50 @@ namespace solpr
             db = new ParkDBEntities();
             loadCompStatus();
             loadEmployees();
+            loadComponents();
+            refreshList();
+            int index = Program.mf.dataGridView1.SelectedRows[0].Index;
+            int id = 0;
+            bool converted = Int32.TryParse(Program.mf.dataGridView1[0, index].Value.ToString(), out id);
+            if (converted == false)
+                return;
+            Computer pc = db.Computers
+                   .Where(p => p.Id == id)
+                   .FirstOrDefault();
+            Employee emplo = db.Employees
+                   .Where(p => p.Id == pc.EmployeeId)
+                   .FirstOrDefault();
+           
+
+            comboBox7.SelectedValue = emplo.Id;
+            comboBox8.SelectedValue = pc.Status;
+
+          
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
+            using (db = new ParkDBEntities())
+            {
+                int index = Program.mf.dataGridView1.SelectedRows[0].Index;
+                int id = 0;
+                bool converted = Int32.TryParse(Program.mf.dataGridView1[0, index].Value.ToString(), out id);
+                if (converted == false)
+                    return;
+                Computer pc = db.Computers
+                        .Where(p => p.Id == id)
+                        .FirstOrDefault();
+                //Employee emplo = db.Employees
+                //       .Where(p => p.Id == pc.EmployeeId)
+                //       .FirstOrDefault();
 
+                pc.Status = (ComputerStatus)comboBox8.SelectedValue;
+                pc.EmployeeId = (int)comboBox7.SelectedValue;
+                //db.Computers.Add(pc);
+                db.SaveChanges();
+                Close();
+                Program.mf.RefreshComputersGrid();
+            }
         }
 
         private void button2_Click(object sender, EventArgs e)
@@ -65,6 +104,58 @@ namespace solpr
             comboBox7.DisplayMember = "sad";
             comboBox7.ValueMember = "gov";
 
+        }
+        public void loadComponents()
+        {
+            comboBox1.DataSource = Enum.GetValues(typeof(ComponentType))
+              .Cast<Enum>()
+              .Select(value => new
+              {
+                  (Attribute.GetCustomAttribute(value.GetType().GetField(value.ToString()), typeof(DescriptionAttribute)) as DescriptionAttribute).Description,
+                  value
+              })
+              .OrderBy(item => item.value)
+              .ToList();
+            comboBox1.DisplayMember = "Description";
+            comboBox1.ValueMember = "value";
+        }
+        public void refreshList()
+        {
+            dataGridView2.DataSource = dataGridView1.DataSource;
+            foreach (DataGridViewRow row in dataGridView2.Rows)
+            {
+                //row.re
+            }
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            var result = from comps in db.Components
+                         join manufac in db.Manufacturers on comps.ManufacturerId equals manufac.Id
+                         join specs in db.Specs on comps.Id equals specs.ComponentId
+                         select new
+                         {
+                             ID = comps.Id,
+                             Тип = comps.Type,
+                             Модель = comps.Model,
+                             Производитель = manufac.Name,
+                             Характеристики = specs.Name + "−" + specs.Value
+                         };
+            var componentsFilter = result.AsEnumerable();
+            componentsFilter = componentsFilter.Where(x => x.Тип.ToString() == comboBox1.SelectedValue.ToString());
+            dataGridView1.DataSource = componentsFilter.ToList();
+            dataGridView1.Refresh();
+        }
+
+        private void dataGridView1_SelectionChanged(object sender, EventArgs e)
+        {
+            if (dataGridView1.SelectedRows.Count > 0)
+            {
+                dataGridView2.Rows.Add(dataGridView1.SelectedRows[0]);
+                dataGridView1.Rows.Remove(dataGridView1.SelectedRows[0]);
+                dataGridView1.Refresh();
+                dataGridView2.Refresh();
+            }
         }
     }
 }
